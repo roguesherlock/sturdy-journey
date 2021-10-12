e4b3140b7bd76dde741460a25f13dd1f4533b106
Akash <aakash@hey.com>
2021-10-12 16:39:58 +0530

    add product show page

---
+++ src/components/Review/Review.tsx
@@ -0,0 +1,40 @@
+import { StarIcon } from "@heroicons/react/solid";
+import { classNames } from "src/lib/utils";
+type Props = {
+  review: Review;
+};
+
+export const Layout = ({ review }: Props) => {
+  return (
+    <>
+      <div className="flex items-center gap-5">
+        <dt className="flex items-center gap-2">
+          <div className="flex items-center">
+            <div className="flex items-center">
+              <div className="flex items-center">
+                {[...Array(5)].map((_, i) => (
+                  <StarIcon
+                    key={i}
+                    className={classNames(
+                      i >= Number(review.rating)
+                        ? "text-gray-200"
+                        : "text-yellow-400",
+                      " h-8 w-8"
+                    )}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          <p className="text-gray-900 font-bold">
+            {review.rating}
+            <span className="sr-only"> out of 5 stars</span>
+          </p>
+        </dt>
+        <dd className="flex gap-2 text-gray-500">{review.description}</dd>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from "next";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { getProduct } from "src/lib/api";
+import Layout from "src/components/Layout";
+import { StarIcon } from "@heroicons/react/solid";
+import { classNames } from "src/lib/utils";
+import Review from "src/components/Review";
+import Button from "src/components/Button";
+
+const Home: NextPage<{ product: Product }> = ({ product }) => {
+  const averageRating = Number(
+    product.reviews!.reduce((acc, cur) => acc + cur.rating, 0) /
+      product.reviews!.length
+  ).toPrecision(2);
+  return (
+    <Layout>
+      <div className="max-w-screen-sm mx-auto py-12">
+        <div className="space-y-6">
+          <h1 className="text-4xl font-extrabold text-gray-900">
+            {product.name}
+          </h1>
+          <div className="flex items-center justify-between text-xl">
+            <div>
+              <h2 className="sr-only">Reviews</h2>
+              <div className="flex gap-2 items-center">
+                <p className="text-gray-900 font-medium text-3xl">
+                  {averageRating}
+                  <span className="sr-only"> out of 5 stars</span>
+                </p>
+                <div className="flex items-center">
+                  {[...Array(5)].map((_, i) => (
+                    <StarIcon
+                      key={i}
+                      className={classNames(
+                        i >= Number(averageRating)
+                          ? "text-gray-200"
+                          : "text-yellow-400",
+                        " h-8 w-8"
+                      )}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div>
+              <Button label="Add review" />
+            </div>
+          </div>
+        </div>
+        <div className="border border-t border-gray-200 mt-8"></div>
+        <div className="mt-8">
+          <div className="px-4 py-5 sm:p-0">
+            <h1 className="text-3xl font-bold text-gray-900">Reviews</h1>
+            <dl className="space-y-4 mt-6">
+              {product.reviews!.map((review) => (
+                <Review key={review.id} review={review} />
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const product: Product = await getProduct(context.params?.id as string);
+  return {
+    props: {
+      product,
+    },
+  };
+};
