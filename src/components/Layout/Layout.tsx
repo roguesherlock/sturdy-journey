e9687e921223c89330bc1849cc7a25a8ca404475
Akash <aakash@hey.com>
2021-10-12 16:03:26 +0530

    add layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <div className="flex flex-col gap-4 min-h-screen min-w-screen bg-gray-50 text-gray-900">
+        <header className="flex items-center justify-between px-4 sm:px-6 lg:px-8 py-4"></header>
+        <main className="flex-grow max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
+          {children}
+        </main>
+        <footer className="mt-auto">
+          <div className="max-w-7xl mx-auto overflow-hidden px-4 sm:px-6 lg:px-8 py-4">
+            <div className="mt-8 text-xl flex gap-2 items-center justify-center">
+              <p>Gumroad</p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
